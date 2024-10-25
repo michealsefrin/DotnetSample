8dd93a6fa9e7889ea51b188398b761376a5fbbe4
Micheal Sefrin <michealsefrin98@gmail.com>
2024-10-26 00:27:07 +0300

    Commit

---
+++ Sample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace consolesample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to my wolrd");
+      
+            Console.Write("please enter your name:");
+
+            string username = Console.ReadLine();
+
+            Console.WriteLine($"Hello {username}! nice to meet you");
+            Console.WriteLine("press any key to exit");
+            Console.ReadLine();
+        }
+    }
+
+}
